ad42d39f342091f6d6a45c8226a007181e741d1a
nchaliki <nirmala.chaliki@hermes-europe.co.uk>
2020-02-06 17:04:46 +0000

    Refactored

--- src/test/java/ApiTest.java
+++ src/test/java/ApiTest.java
@@ -1,9 +1,7 @@
 import cucumber.api.CucumberOptions;
-import cucumber.api.Scenario;
-import cucumber.api.java.After;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
-import stepDefinitions.Hooks;
+
 
 import java.util.logging.Logger;
 
@@ -16,6 +14,4 @@ import java.util.logging.Logger;
         tags = {"@ApiTests", "~@FeatureAutomationTest", "~@ManualTests"}
 )
 public class ApiTestRunner {
-
-    public static Logger log = Logger.getLogger(String.valueOf(ApiTestRunner.class));
 }
